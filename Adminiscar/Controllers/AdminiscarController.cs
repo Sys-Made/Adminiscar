39aa9a4e60d210e7c42bfcb67ca4cf831d1a4f22
Patric Alison <alisson_gamer2000@outlook.com>
2020-11-15 16:00:12 -0300

    add AdminicarControler

---
+++ Adminiscar/Controllers/AdminiscarController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adminiscar.Controllers
+{
+    public class AdminiscarController : Controller
+    {
+        // GET: Adminiscar
+        /*public ActionResult Index()
+        {
+
+            return View();
+
+            
+        }*/
+
+        public string Index() {
+
+            return "esse é minha pagina default action";
+
+        }
+
+        public string Welcome() {
+
+            return "esse é meu method Welcome action";
+
+        }
+    }
+}
