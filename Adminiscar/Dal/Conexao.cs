a07f8dd667bdc3c6b75b2ab1f5e0d61b095fc96c
Patric Alison <alisson_gamer2000@outlook.com>
2021-06-08 10:51:57 -0300

    add arq de login e conexao

---
+++ Adminiscar/Dal/Conexao.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Adminiscar.Dal
+{
+    public class Conexao
+    {
+        //Fazendo a conexao do banco
+        MySqlConnection conx = new MySqlConnection("Server=localhost;DataBase=bdadminiscar;User=root;pwd=40028922+pokpok"); //variavel com a conexao
+
+        public static string msg; // mensagem de erro
+
+        //conectando 
+        public MySqlConnection MyConectorBd() {
+
+            try
+            {
+
+                conx.Open();
+
+            }
+            catch(Exception erro) {
+
+                msg = "Ocorreu um erro na conexão com o banco " + erro.Message;
+
+            }
+
+            return conx;
+
+        }
+
+        //desconectando
+        public MySqlConnection MyCloseBd(){
+
+            try {
+
+                conx.Close();
+
+            }
+            catch (Exception erro) {
+
+                msg = "Ocorreu um erro ao fechar o banco " + erro.Message;
+
+            }
+
+            return conx;
+
+        }
+
+    }
+}
---
+++ Adminiscar/Models/LoginACS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adminiscar.Models
+{
+    public class LoginACS
+    {
+        public string user { get; set; }
+        public string pass { get; set; }
+        public string tipoUser { get; set; }
+
+    }
+}
