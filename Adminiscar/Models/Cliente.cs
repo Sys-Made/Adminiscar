f73c0a7296718a6652ff189ddb3b1170cc798727
Patric Alison <alisson_gamer2000@outlook.com>
2021-06-09 10:19:09 -0300

    add Class CLiente e construindo o cadastro

---
+++ Adminiscar/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adminiscar.Models
+{
+    public class Cliente
+    {
+
+        public string codCli { get; set; }
+        public string nomeCli { get; set; }
+        public string cpfCli { get; set; }
+        public string cnpjCli { get; set; }
+        public string cnhCli { get; set; }
+        public string tellCli { get; set; }
+        public string endCli { get; set; }
+
+    }
+}
