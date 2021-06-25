334358022b60610809f9049b6490e71248a13935
Patric Alison <alisson_gamer2000@outlook.com>
2021-06-25 16:28:51 -0300

    funcao e tela locacao v0.0.300

---
+++ Adminiscar/Dal/acoesLoc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Adminiscar.Models;
+using MySql.Data.MySqlClient;
+
+namespace Adminiscar.Dal
+{
+    public class acoesLoc
+    {
+        
+        Conexao conect = new Conexao(); //istanciando o meu objeto classe conect
+
+    }
+}
---
+++ Adminiscar/Models/Locacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adminiscar.Models
+{
+    public class Locacao
+    {
+
+        public string numPedLoc { get; set; }
+        public string nameEmpLoc { get; set; }
+        public string cnpjLoc { get; set; }
+        public string cpfLoc { get; set; }
+        public string cnhLoc { get; set; }
+        public string nomeCarLoc { get; set; }
+        public string catCar { get; set; }
+        public string placCar { get; set; }
+        public string dataTempo { get; set; }
+
+    }
+}
